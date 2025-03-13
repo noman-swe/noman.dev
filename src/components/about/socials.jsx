99ab85f348c1576257f2c95e18a4cf321e0951f5
noman <noman.swe130@gmail.com>
2025-03-13 23:54:49 +0600

    [projects, about us and social component added]

---
+++ src/components/about/socials.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+	faTwitter,
+	faGithub,
+	faLinkedin,
+	faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+
+import info from "../../data/user";
+
+import "./styles/socials.css";
+
+const Socials = () => {
+	return (
+		<div className="socials">
+			{/* <div className="social">
+				<a href={INFO.socials.twitter} target="_blank" rel="noreferrer">
+					<div className="social-icon">
+						<FontAwesomeIcon
+							icon={faTwitter}
+							className="social-icon"
+						/>
+					</div>
+					<div className="social-text">Follow on Twitter</div>
+				</a>
+			</div> */}
+
+			<div className="social">
+				<a href={info.socials.github} target="_blank" rel="noreferrer">
+					<div className="social-icon">
+						<FontAwesomeIcon
+							icon={faGithub}
+							className="social-icon"
+						/>
+					</div>
+					<div className="social-text">Follow on GitHub</div>
+				</a>
+			</div>
+
+			<div className="social">
+				<a
+					href={info.socials.linkedin}
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className="social-icon">
+						<FontAwesomeIcon
+							icon={faLinkedin}
+							className="social-icon"
+						/>
+					</div>
+					<div className="social-text">Follow on LinkedIn</div>
+				</a>
+			</div>
+
+			{/* <div className="social">
+				<a
+					href={INFO.socials.instagram}
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className="social-icon">
+						<FontAwesomeIcon
+							icon={faInstagram}
+							className="social-icon"
+						/>
+					</div>
+					<div className="social-text">Follow on Instagram</div>
+				</a>
+			</div> */}
+			<div className="social">
+				<a
+					href={info.socials.facebook}
+					target="_blank"
+					rel="noreferrer"
+				>
+					<div className="social-icon">
+						<FontAwesomeIcon
+							icon={faFacebook}
+							className="social-icon"
+						/>
+					</div>
+					<div className="social-text">Follow on Facebook</div>
+				</a>
+			</div>
+
+			<div className="email">
+				<div className="email-wrapper">
+					<a
+						href={`mailto:${info.main.email}`}
+						target="_blank"
+						rel="noreferrer"
+					>
+						<div className="social-icon">
+							<FontAwesomeIcon icon={faEnvelope} />
+						</div>
+
+						<div className="social-text">{info.main.email}</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Socials;
